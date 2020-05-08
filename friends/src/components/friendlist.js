eafda86df896d30d2f733dc1ab147ae75484ff03
Jennifer Weiner <58009503+weinerjm14@users.noreply.github.com>
2020-05-08 18:52:14 -0400

    retrieving and displaing friends list working

--- friends/src/components/friendlist.js
+++ friends/src/components/friendlist.js
@@ -1,9 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { axiosWithAuth } from "../util/axiosWithAuth";
 
 export function FriendList() {
+  const [friends, setFriends] = useState([]);
+  useEffect(() => {
+    axiosWithAuth()
+      .get("/friends", localStorage.getItem("token"))
+      .then(res => setFriends(res.data))
+      .catch(err => console.log("error getting data:", err));
+  }, []);
   return (
     <section className="flcontainer">
-      <h2>Hi, From Friends List</h2>
+      <h2>Your Friends</h2>
+      {friends ? (
+        friends.map(friend => {
+          return (
+            <section className="friend" key={friend.id}>
+              <h3>{friend.name}</h3>
+              <p>Age: {friend.age}</p>
+              <p>Email: {friend.email}</p>
+            </section>
+          );
+        })
+      ) : (
+        <h2>Finding Friends</h2>
+      )}
     </section>
   );
 }
