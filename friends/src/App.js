190272108a53758e71f578612aeee5aca3fda860
Jennifer Weiner <58009503+weinerjm14@users.noreply.github.com>
2020-05-08 17:38:21 -0400

    work on login

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,9 +1,33 @@
 import React from "react";
-
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import PrivateRoute from "./components/privateRoute";
 import "./App.css";
 
+import { Login } from "./components/login";
+import { FriendList } from "./components/friendlist";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <section className="App">
+      <Router>
+        <div className="App">
+          <ul>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/friendslist">Friends List</Link>
+            </li>
+          </ul>
+          <Switch>
+            <PrivateRoute exact path="/friendslist" component={FriendList} />
+            <Route path="/login" component={Login} />
+            <Route component={Login} />
+          </Switch>
+        </div>
+      </Router>
+    </section>
+  );
 }
 
 export default App;
---
+++ friends/src/components/friendlist.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export function FriendList() {
+  return (
+    <section className="flcontainer">
+      <h2>Hi, From Friends List</h2>
+    </section>
+  );
+}
